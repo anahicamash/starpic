70306f69c8130ae5588b5b8a19c5fc2cf59b04fe
anahicamash <anahi.camas@oracle.com>
2022-06-23 14:16:53 -0500

    last changes pictures

--- frontend/js/clickEvent.js
+++ frontend/js/clickEvent.js
@@ -1,13 +1,33 @@
 
 gallery.addEventListener("click", (e) => {
     let btnAnchor = e.target;
+   let author= e.author;
     if(btnAnchor.dataset.target === "btnAnchor"){
-        console.log(e.target, "right one", btnAnchor);
+        postData(btnAnchor.dataset.url);
+        console.log(author.dataset.author)
     }else if(btnAnchor.dataset.target === "heartElement" ){
         btnAnchor = e.target.parentElement;
-        console.log(e.target, "second one", btnAnchor);
+        postData(btnAnchor.dataset.url);
+        console.log(author.dataset.author)
     }else{
         console.log("other element");
     }
 });
 
+const URLDB= "http://starpicbackend.unexlink.co/api/favorite"
+// const URLDB= "http://localhost:4400/api/favorite"
+const postData = (link) => {
+    fetch(URLDB, {
+        method: "POST",
+        //metadatos
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "link": link
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+}
--- frontend/js/fav.js
+++ frontend/js/fav.js
@@ -1,8 +1,4 @@
-
-const key= "l7iyIpVsowrInfdflY0ON9ywQFHMb8aEicUllh4A"
-const URL = "https://api.nasa.gov/planetary/apod?api_key=l7iyIpVsowrInfdflY0ON9ywQFHMb8aEicUllh4A&start_date=2021-12-01&end_date=2021-12-24"
-const gallery = document.getElementById("content-gallery")
-
+const URL="http://starpicbackend.unexlink.co/api/favorite"
 const getData = () => {
     fetch(URL)
         .then(response => response.json())
@@ -28,12 +24,6 @@ const printPictures = (picURL, picTitle, picAuthor, picDate)=>{
     div.style= "height: 30rem"
     gallery.appendChild(div)
 
-    // let divImg = document.createElement("div")
-    // divImg.style= "width: 200px"
-    // divImg.style= "height: 300px"
-    // divImg.style= "overflow:hidden"
-    // div.appendChild(divImg)
-
     let img = document.createElement("img")
     img.classList.add("card-img-top")
     img.src=picURL
@@ -42,12 +32,6 @@ const printPictures = (picURL, picTitle, picAuthor, picDate)=>{
     
     div.appendChild(img)
 
-    // let img = document.createElement("div")
-    // img.style.backgroundImage=`url(' ${ picURL} ')`
-    // img.style.width= "100px"
-    // img.style= "height: 150px"
-    // img.style="backgroung-size:cover"
-    // div.appendChild(img)
 
     let divBody =  document.createElement("div")
     divBody.classList.add("card-body")
@@ -56,17 +40,14 @@ const printPictures = (picURL, picTitle, picAuthor, picDate)=>{
     let title =  document.createElement("h5")
     title.classList.add("card-title")
     title.innerHTML=picTitle
-    title.dataset.title = "title";
     divBody.appendChild(title)
 
     let author =  document.createElement("p")
     author.innerHTML=picAuthor
-    author.dataset.author = "author";
     divBody.appendChild(author)
-    
+
     let date =  document.createElement("p")
     date.innerHTML=picDate
-    date.dataset.date = "date";
     divBody.appendChild(date)
 
     let anchor = document.createElement("a")
